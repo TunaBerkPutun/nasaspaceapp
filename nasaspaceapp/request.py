1689dcdbbb41feaa83165b157796f719c15a84a0
TunaBerkPutun <tunaberkputun.tbp@gmail.com>
2020-10-03 00:44:24 +0300

    düzenlemeeeee

--- nasaspaceapp/request.py
+++ nasaspaceapp/request.py
@@ -4,7 +4,7 @@ API_KEY = "ec579e6e855b4fac41b29fd43e90c257"
 URL = "http://api.openweathermap.org/data/2.5/weather"
 DEFAULT_UNIT = "metric"
 DEFAULT_LANG = "en"
-CITY_ID = "745042"
+CITY_ID = "745042" #istanbulun id'si
 
 class Weather:
     def __init__(self, key, id, url, unit = DEFAULT_UNIT, lang = DEFAULT_LANG):
@@ -13,13 +13,7 @@ class Weather:
 
     def returnWeather(self):
         return self.r
+
         
-
-
-"""def getCurrentWeather(key, id, url, unit = DEFAULT_UNIT, lang = DEFAULT_LANG):
-    PARAMS = {"id": id, "appid": key, "unit": unit, "lang": lang}
-    r = requests.get(url = url, params=PARAMS)
-    return r.json()"""
-
 hava = Weather(API_KEY, CITY_ID, URL)
 print(hava.returnWeather())
